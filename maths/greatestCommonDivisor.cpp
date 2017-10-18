16cde68e06e25c26203c957a36f35783460438fb
Agrawal, Shubham Ajay <shubhagr@visa.com>
2017-10-18 09:18:04 +0530

    Added GCD function

---
+++ maths/greatestCommonDivisor.cpp
@@ -0,0 +1,23 @@
+/**
+Author: Shubham Agrawal
+email: <shubham.agrawal.8025@gmail.com>
+*/
+
+/*Greatest Common Divisor*/
+
+#include<iostream>
+using namespace std;
+
+int calculateGCD(int a, int b){
+if(b==0)
+return a;
+return calculateGCD(b,a%b);
+}
+
+int main(){
+
+/*call gcd function*/
+int gcd = calculateGCD(182,65);
+cout<<gcd<<endl;
+return 0;
+}
